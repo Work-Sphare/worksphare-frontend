cb4bd94d765f256b372bd9d6758cb4660fe14cbc
RLogic2025 <rigelogic.systems@gmail.com>
2026-07-09 12:22:19 +0530

    chore: reinitialize frontend with clean Vite setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
-import './index.css'
-import React,{ StrictMode } from 'react'
+import React from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './app/App'
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
